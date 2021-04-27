f0baffe6076d8eac6f4a44d9e716054fbe0c411b
Ivan Bakaidov <ivan@aacidov.ru>
2021-04-27 13:33:48 +0300

    fix gclass type

--- lib/GClass.d.ts
+++ lib/GClass.d.ts
@@ -1,5 +1,5 @@
 declare const classes: readonly ["BC1", "BC2", "BC3", "BC4", "ПBC3", "ПBC4", "ТBC1/BC2"];
-export declare type gclass = typeof classes;
+export declare type gclass = typeof classes[number];
 export declare class GClass {
     static get classes(): readonly ["BC1", "BC2", "BC3", "BC4", "ПBC3", "ПBC4", "ТBC1/BC2"];
 }
--- src/GClass.ts
+++ src/GClass.ts
@@ -1,6 +1,6 @@
-const  classes = ['BC1', 'BC2', 'BC3', 'BC4', 'ПBC3', 'ПBC4', 'ТBC1/BC2'] as const;
-export type gclass = typeof classes
+const classes = ['BC1', 'BC2', 'BC3', 'BC4', 'ПBC3', 'ПBC4', 'ТBC1/BC2'] as const;
+export type gclass = typeof classes[number]
 
-export class GClass{
-    static get classes (){return classes}
+export class GClass {
+    static get classes() { return classes }
 }
