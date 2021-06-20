ca27900854925d4f02fc64e81663c875c0ebb696
Ivan Bakaidov <ivan@aacidov.ru>
2021-06-20 14:38:24 +0300

    add bc5

--- lib/GClass.d.ts
+++ lib/GClass.d.ts
@@ -1,6 +1,6 @@
-declare const classes: readonly ["BC1", "BC2", "BC3", "BC4", "ПBC3", "ПBC4", "ТBC1/BC2"];
+declare const classes: readonly ["BC1", "BC2", "BC3", "BC4", "BC5", "ПBC3", "ПBC4", "ПBC5", "ТBC1/BC2"];
 export declare type gclass = typeof classes[number];
 export declare class GClass {
-    static get classes(): readonly ["BC1", "BC2", "BC3", "BC4", "ПBC3", "ПBC4", "ТBC1/BC2"];
+    static get classes(): readonly ["BC1", "BC2", "BC3", "BC4", "BC5", "ПBC3", "ПBC4", "ПBC5", "ТBC1/BC2"];
 }
 export {};
--- src/GClass.ts
+++ src/GClass.ts
@@ -1,4 +1,4 @@
-const classes = ['BC1', 'BC2', 'BC3', 'BC4', 'ПBC3', 'ПBC4', 'ТBC1/BC2'] as const;
+const classes = ['BC1', 'BC2', 'BC3', 'BC4', 'BC5', 'ПBC3', 'ПBC4', 'ПBC5', 'ТBC1/BC2'] as const;
 export type gclass = typeof classes[number]
 
 export class GClass {
